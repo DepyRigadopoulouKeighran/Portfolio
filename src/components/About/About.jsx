6cd2649b8084d8fd00695279de4af5b59ef2a7b8
Depy Rigadopoulou-Keighran <136313917+DepyRigadopoulouKeighran@users.noreply.github.com>
2026-02-18 17:27:08 +0100

    Update About to German

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,34 +8,20 @@ const AboutPage = () => {
       className="flex flex-grow-0 justify-start flex-row-reverse items-center filter-about min-h-screen "
     >
       <div className=" w-1/3 mx-6 mr-20">
-        <h2 className="text-6xl mt-36 titleAbout">About</h2>
+        <h2 className="text-6xl mt-36 titleAbout">Profil</h2>
         <br />
         <p className="text-2xl mt-4 mb-16 text-justify tracking-wider contentAbout">
-          Greetings, Earthlings! I'm Depy, your friendly neighborhood web
-          development wizard, here to sprinkle a little magic onto your digital
-          endeavors.
+          Ich bin angehende Webentwicklerin mit Kenntnissen sowohl im Frontend als 
+          auch im Backend.  
+          Ich habe Erfahrung mit HTML, CSS, JavaScript sowie grundlegenden Backend 
+          Technologien.
           <br />
           <br />
-          Ever wonder what happens when you mix a dash of marketing mojo with a
-          hefty dose of tech savvy? Well, you get me—a full-stack web developer
-          extraordinaire with a knack for turning ideas into digital gold.
+          Ich interessiere mich besonders für die Entwicklung moderner und 
+          benutzerfreundlicher Webanwendungen.
           <br />
-          My journey into the world of web development wasn't exactly your
-          typical tale. It involved a fair amount of curiosity, a sprinkle of
-          risk-taking, and a whole lot of caffeine. But hey, isn't that how all
-          the best stories start?
-          <br />
-          <br />
-          Beyond the pixels and code, I'm all about promoting a positive work
-          environment and championing diversity like it's going out of style.
-          Because let's face it, the digital landscape could use a little more
-          love and understanding.
-          <br />
-          <br />
-          So, if you're ready to embark on a digital adventure filled with
-          creativity, collaboration, and maybe a few corny jokes along the way,
-          I'm your guy! Coding with a dash of humor – because laughter is the
-          best debugging tool.
+          Mein Ziel ist es, praktische Erfahrung zu sammeln und mich sowohl fachlich 
+          als auch persönlich weiterzuentwickeln.
           <br />
           <br />
         </p>
